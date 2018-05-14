5f410403193f106f4cfaa3225b5395d51a40080d
messense <messense@icloud.com>
2018-05-14 19:31:56 +0800

    Add systemd service switch

--- homekit.py
+++ homekit.py
@@ -8,6 +8,9 @@ from pyhap.const import CATEGORY_SENSOR, CATEGORY_SWITCH
 from pyhap.accessory import Bridge, Accessory, AsyncAccessory
 from pyhap.accessory_driver import AccessoryDriver
 
+from systemd_dbus.exceptions import SystemdError
+from systemd_dbus.manager import Manager as SystemdManager
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -50,11 +53,52 @@ class ShutdownSwitch(Accessory):
             os.system("sudo shutdown -h now")
 
 
+class SystemdServiceSwitch(Accessory):
+    '''A switch accessory that manages Systemd services'''
+    category = CATEGORY_SWITCH
+
+    manager = SystemdManager()
+
+    def __init__(self, *args, service=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not service.endswith('.service'):
+            service = service + '.service'
+        try:
+            self.unit = self.manager.get_unit(service)
+        except SystemdError:
+            logger.warning('Get %s unit error', service)
+            return
+
+        is_on = self.unit.properties.ActiveState == 'active' and self.unit.properties.SubState == 'running'
+        serv_switch = self.add_preload_service('Switch')
+        self.char_on = serv_switch.configure_char(
+            'On',
+            value=is_on,
+            setter_callback=self.toggle_service
+        )
+
+    def toggle_service(self, value):
+        try:
+            if value:
+                self.unit.start('fail')
+            else:
+                self.unit.stop('fail')
+        except SystemdError:
+            logger.exception('Toggle Systemd service failed')
+
+
 def get_bridge():
     bridge = Bridge(display_name='Raspberry Pi')
     temp_sensor = RpiTemperatureSensor('Pi Temperature Sensor')
     bridge.add_accessory(temp_sensor)
     bridge.add_accessory(ShutdownSwitch('Shutdown Pi'))
+
+    services = [
+        ('netdata', 'netdata monitor'),
+        ('frp', 'frp proxy'),
+    ]
+    for service, display_name in services:
+        bridge.add_accessory(SystemdServiceSwitch(display_name, service=service))
 
     return bridge
 
